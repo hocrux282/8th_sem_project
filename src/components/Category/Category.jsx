88b72fdbc9c547070e64f1e21377e3a59a81a2d5
Yash Srivastava <75997494+hocrux282@users.noreply.github.com>
2023-03-06 21:50:32 +0530

    Second commit

--- src/components/Category/Category.jsx
+++ src/components/Category/Category.jsx
@@ -4,7 +4,7 @@ const Category = () => {
     return <div className="category-main-content">
         <div className="layout">
             <div className="category-title">
-                Category Title
+                Category Title Yash
             </div>
             <Products innerPage={true}/>
         </div>
